1fc1ee42bc9b7854d0db341b7be49107bfc5d24e
AdithyaPNCodilar <adithya.pn@codilar.com>
2023-03-16 18:07:47 +0530

    getelementbyid removed

--- superglobals.php
+++ superglobals.php
@@ -1,14 +1,19 @@
 <?php
 
-function appendData(){
-
+/**
+ * This file contains the MyClass class.
+ *
+ * @author Adithya P N<adithya.pn@codilar.com>
+ * @return mixed
+ */
+function saveData()
+{
     $data = file_get_contents('data.json');
     $registrations = json_decode($data, true);
     $image = $_FILES['image']['name'];
     $temp_file = $_FILES['image']['tmp_name'];
 
     if ($_POST != null) {
-
         $registration = array(
             'name' => $_POST['name'],
             'email' => $_POST['email'],
@@ -17,22 +22,18 @@ function appendData(){
         );
         array_push($registrations, $registration);
 
-
         $data = json_encode($registrations, JSON_PRETTY_PRINT);
         move_uploaded_file($temp_file, "uploads/" . $image);
 
         file_put_contents('data.json', $data);
 
         echo 'Registration successful';
-    } 
-    else {
+    } else {
         echo 'Registration Unsuccessful';
     }
-
 }
 
-if(file_get_contents('data.json') == null) {
+if (file_get_contents('data.json') == null) {
     file_put_contents('data.json', '[]');
 }
-appendData();
-?>
+saveData();
